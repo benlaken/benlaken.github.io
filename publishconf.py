85c9200b3adfabfb89d212738738571241d39355
Benjamin A. Laken <benlaken@gmail.com>
2016-09-23 14:32:39 +0100

    trying to get travis running a build

--- publishconf.py
+++ publishconf.py
@@ -18,6 +18,9 @@ CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
+EXTRA_PATH_METADATA = {'extra/travis.yml': {'path': '.travis.yml'}}
+
+
 # Following items are often useful when publishing
 
 #DISQUS_SITENAME = ""
